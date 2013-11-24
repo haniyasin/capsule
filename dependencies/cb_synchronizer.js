c7025696af357cc71bf8b3ca72e5cad4d45a359d
Nikita Zaharov aka ix <ix@2du.ru>
2013-11-24 14:59:55 +0600

    implemented a library for calling a some callback after many other callbacks was called

---
+++ dependencies/cb_synchronizer.js
@@ -0,0 +1,27 @@
+exports.create = function(){
+    var callbacks = [];
+    return {
+	"add" : function(callback, how_many){
+	    if(how_many == undefined)
+		how_many = 1;
+	    callbacks.push([callback, how_many]);
+	    var parent = this;
+	    return function(){
+		for (ind in callbacks){
+		    if(callbacks[ind][0] == callback){
+			var how_many = callbacks[ind][1] -= 1;
+			if(how_many == 0){
+			    callbacks.splice(ind,1);
+			}
+		    }
+		}
+		callback.apply(this, Array.prototype.reverse.call(arguments));
+		if(!callbacks.length)
+		    parent.after_all();
+	    }
+	},
+
+	"after_all" : function(callback){
+	}
+    }
+}
