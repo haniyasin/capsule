4e1ac8ffe62f8135c986e85daeb39173bc158cc2
Nikita Zaharov aka ix <ix@2du.ru>
2014-03-19 14:31:02 +0600

    destroy method and destroy callback implemented in transport/direct

--- modules/transport/direct.js
+++ modules/transport/direct.js
@@ -6,44 +6,65 @@ exports.create = function(context, features, capsule){
 	callbacks[context.url] = new Array();
     if(features & transport.features.server){
         return {
-	    on_connect : function(callback){
+	    "on_connect" : function(callback){
 		if(callback != null)
 		    callbacks[context.url][0] = function(uuid){
 			callback({
-				     on_msg : function (callback){
-					 callbacks[context.url]["s" + uuid] = callback;
+				     "on_msg" : function (callback){
+					 callbacks[context.url]['s' + uuid] = callback;
 				     },
-				     send : function(msg){
+				     "send" : function(msg){
 					 if(typeof(callbacks[context.url]["c" + uuid]) == 'function')
 					     callbacks[context.url]["c" + uuid](msg);
 					 else console.log("server: Callback on msg from", uuid, "client is not setted");
+				     },
+				     "on_destroy" : function(callback){
+					 callbacks[context.url]['ds'][uuid] = callback;
+				     },
+				     "destroy" : function(){
+					 callbacks[context.url]['s' + uuid] = null;
+
+					 if(callbacks[context.url]['ds'][uuid])
+					     callbacks[context.url]['ds'][uuid]();
 				     }
 				 });
 		    }
 	    },
-	    destroy : function(){
-		callbacks[context.url].splice(0,1);
+	    "destroy" : function(){
+		var destroy_cbs = callbacks[context.url]['d'];
+		for(ind in destroy_cbs){
+		    if(destroy_cbs[ind])
+			destroy_cbs[ind]();		    
+		}
+		callbacks[context.url] = [];
 	    }
 	}		    
     }else if (features & transport.features.client){
 	var uuid = capsule.modules.uuid.generate_str();
 	return {
-	    connect : function(){
+	    "connect" : function(){
 		if(typeof(callbacks[context.url][0]) == 'function')
 		    callbacks[context.url][0](uuid);
 		else console.log("client: Callback on connect is not setted");
 	    },
-	    on_msg : function (callback){
+	    "on_msg" : function (callback){
 		callbacks[context.url]["c" + uuid] = callback;
 	    },
-	    send : function(msg){
+	    "send" : function(msg){
 		if(typeof(callbacks[context.url]['s' + uuid]) == 'function')
 		    callbacks[context.url]['s' + uuid](msg);
 		else console.log("client: Callback on msg is not setted");
 	    },
-	    destroy : function(){
-		delete callbacks[context.url]["c" + uuid];
-	    }	
+	    
+	    "on_destroy" : function(callback){
+		callbacks[context.url]['dc' + uuid] = callback;
+	    },
+	    "destroy" : function(){
+		callbacks[context.url]['c' + uuid] = null;
+		
+		if(callbacks[context.url]['dc' + uuid])
+		    callbacks[context.url]['dc' + uuid]();
+	    }
 	}
     }
 }
