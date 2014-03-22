c5c387b1483ef3b19aa4db2f4db5556b122573d6
Nikita Zaharov aka ix <ix@2du.ru>
2014-03-23 00:51:22 +0600

    http/socket_srv: refactored a little, some comments for better undestaning

--- modules/transport/http/socket_srv.js
+++ modules/transport/http/socket_srv.js
@@ -13,62 +13,76 @@ function response_holder(_incoming, modules){
     var ids = new bb_allocator.create(bb_allocator.id_allocator);
     var responses = [];
     this.delayed_packets = [];
+    var extra_cleaner_timer = null; //extra connection cleaner
+
+    var _packets = this.delayed_packets;
+
     this.get_waited_response = function(cli_id){
 	if(responses.length)
 	    return responses[cli_id].pop();
 	return null;
     }
 
-    var _packets = this.delayed_packets;
-
     this.activate = function(context){
+	extra_cleaner_timer = modules.timer.js.create(
+	    function(){
+		//если много ждёт, то завершаем и оставляем не более 3
+		for(cli_id in responses){				 
+		    while(responses[cli_id].length > 3){
+			responses[cli_id].pop().end();
+		    }
+		}
+	    }, 500, true);
+
 	ids.alloc();
+
 	//нужно выбирать доступный http_responder
 	modules.http_responder.on_recv(context, 
-					     function(content, response){
-						 var _content = JSON.parse(content);
+				       function(content, response){
+					   //проверить активно ли соединение
+					   response.on_close(
+					       function(){
+						   for(key in responses[_content.cli_id]){
+						       if(responses[_content.cli_id][key] == response)
+							   responses[_content.cli_id].splice(key,1);
+						   }
+					       });
 
-						 var connect_type = false;
-						 if(_content.cli_id == 0){
-						     _content.cli_id = ids.alloc();
-						     connect_type = true;
-						 }
-
-						 if(_content.hasOwnProperty('msg'))
-						     _incoming.add(_content);
-
-						 //проверить активно ли соединение
-						 response.on_close(function(){
-								       for(key in responses[_content.cli_id]){
-									   if(responses[_content.cli_id][key] == response)
-									       responses[_content.cli_id].splice(key,1);
-								       }
-								   });
-						 //записать все ожидающие ответы
-						 if(_packets.length){
-						     var packet = get_by_cli_id(_packets, _content.cli_id);
-						     if(packet)
-							 response.end(packet);
-						 } else 
-						     if(connect_type)
-							 response.end(JSON.stringify({"cli_id" : _content.cli_id}));
-						 else {
-						     //если много ждёт, то завершаем и оставляем не более 3
-						     for(cli_id in responses){				 
-							 while(responses[cli_id].length > 3){
-							     responses[cli_id].pop().end();
-							 }
-						     }
-						     if(typeof(responses[_content.cli_id]) == 'undefined')
-							 responses[_content.cli_id] = [];
-						     responses[_content.cli_id].push(response);	     
-						 }
-					     },
-					     function(error){console.log('response_holder is failed', error)})    
+					   var _content = JSON.parse(content);
+					   
+					   //client is connecting, first msg
+					   var msg_connect = false;
+					   if(_content.cli_id == 0){
+					       _content.cli_id = ids.alloc();
+					       msg_connect = true;
+					   }
+					   
+					   //new msg
+					   if(_content.hasOwnProperty('msg'))
+					       _incoming.add(_content);
+					   
+					   //send delayed packet
+					   if(_packets.length){
+					       var packet = get_by_cli_id(_packets, _content.cli_id);
+					       if(packet)
+						   response.end(packet);
+					   } else 
+					       if(msg_connect)
+						   //client is connecting, send allocated cli_id back
+						   response.end(JSON.stringify({"cli_id" : _content.cli_id}));
+					   else {
+					       //nothing to send, save response object on the future
+					       if(typeof(responses[_content.cli_id]) == 'undefined')
+						   responses[_content.cli_id] = [];
+					       responses[_content.cli_id].push(response);	     
+					   }
+				       },
+				       function(error){console.log('response_holder is failed', error)})    
     }
 
     this.deactivate = function(context){
 	modules.http_responder.remove_callback(context);	
+	extra_cleaner_timer.close();
     }
 }
 
@@ -89,7 +103,6 @@ exports.create = function(context, modules){
     var _sender = new packet_sender(_holder);
     
     return {
-	'type' : 'server',
 	'listen' : function(){
 	    _holder.activate(context);
 	},
