b64aa65e2f3300740a5c2f436400ae3f6105219b
Nikita Zaharov aka ix <ix@2du.ru>
2014-03-23 15:12:05 +0600

    socket_cli is refactored, code cleaned and some fixes

--- modules/transport/http/socket_cli.js
+++ modules/transport/http/socket_cli.js
@@ -5,24 +5,41 @@ var cb_synchronizer = require('../../../parts/cb_synchronizer.js');
 var id_allocator = new bb_allocator.create(bb_allocator.id_allocator);
 
 function requests_holder(type, modules){
-    var requests_allocated = 0;
+    var requests = []; 
+    var self = this;
+
+    this.requests_allocated = 0;
     this.success_metr = 0; //counter of success or failed
     this.on_disconnect = function(){};
     this.create_request = function(){
 	//this is hack for limit of several concurent XMLHttpRequest
-	if((type == 'xhr')&&(requests_allocated > 2))
+	if((type == 'xhr')&&(this.requests_allocated > 5))
 	    return null;	    
 	  
 	var request = modules.http_requester.create(type);
-	this.success_metr++;
 
 	request.on_close(function(){
-			     requests_allocated--;
+			     self.requests_allocated--;
 			     if(request.hasOwnProperty('on_destroy'))
 				 request.on_destroy();
 			  });
-	requests_allocated++;
+
+	request.on_error(function(e){
+			     if(--self.success_metr == 1){
+				 self._lpoller.deactivate();
+				 self.on_disconnect(e);
+			     }
+			 });
+
+	this.requests_allocated++;
+	this.success_metr++;
+
 	return request;
+    }
+
+    this.destroy = function(request){
+	request.close();
+	this.requests_allocated--;
     }
 
     this.close_all_requests = function(){
@@ -32,31 +49,38 @@ function requests_holder(type, modules){
     }
 }
 
+function incoming_processor(context){
+    var _on_recv = function(){};
+
+    this.on_recv = function(callback){
+	_on_recv = callback;
+    }
+
+    this.get_process_msg = function(request, _holder){
+	return function(data){	    
+	    //ignoring reply without data and undefined reply
+	    if(data != undefined && data != 'undefined' && data.length > 0){
+		var pdata = JSON.parse(data);
+		if(pdata.hasOwnProperty('cli_id'))//this is answer on connect msg with allocated cli_id by server
+		    context.cli_id = pdata.cli_id;
+//		console.log(pdata);
+		if(pdata.hasOwnProperty('msg'))
+		    _on_recv(pdata.msg);
+		else
+		    _on_recv(null); //on_connect
+	    }
+	    _holder.destroy(request); //в будущем надо учесть переиспользование объекта, возможно:)
+	}
+    }
+}
+
 function packet_sender(context, _holder, _incoming, _lpoller, modules){
     this.send = function(msg){	
 	var request = _holder.create_request();
 	var msg_json = JSON.stringify({'cli_id' : context.cli_id, 'msg' : msg});
-	//изначальная идея совместить возможность long pooling с задёрженной отправкой не выходит, надо переработать
 	if(request){
-	    request.on_recv(function(data){
-				//ignoring reply without data and undefined reply
-				if(data != undefined && data != 'undefined' && data.length > 0){
-				    var pdata = JSON.parse(data);
-				    if(pdata.hasOwnProperty('cli_id'))
-					context.cli_id = pdata.cli_id;
-				    if(pdata.hasOwnProperty('msg'))
-					_incoming.add(pdata.msg);
-				    else
-					_incoming.add(null); //on_connect
-				}
-				request.close();
-			    });
-	    request.on_error(function(e){
-				 if(--_holder.success_metr == 1){
-				     _lpoller.stop();
-				     _holder.on_disconnect(e);
-				 }
-			     });
+	    request.on_recv(_incoming.get_process_msg(request, _holder));
+
 	    request.open(context);
 	    request.send(msg_json);	    	    
 	}
@@ -67,37 +91,26 @@ function packet_sender(context, _holder, _incoming, _lpoller, modules){
 }
 
 function lpoller(context, _holder, _incoming, modules){
-    var _timer = null;
+    var poll_timer = null;
     this.delayed_packets = [];
     var _packets = this.delayed_packets;
-    var _lpoller = this;
+    var self = this;
     var request = null;
+
     this.try_poll = function(){	
-	if(!_timer){
-	    _timer = modules.timer.js.create(function(){
+	if(!poll_timer){
+	    poll_timer = modules.timer.js.create(function(){
 						 if(request)
-						     return; //request not needed, becouse current still alive
+						     return; //the new request is not needed, because current still alive
 						 var request = _holder.create_request();
 						 if(request){
 						     request.on_destroy = function(){
 							 //console.log('eeee');
 							 request = null;
 						     };
-						     request.on_recv(function(data){
-									 if(data != undefined && data != 'undefined' && data.length > 0){
-									     var pdata = JSON.parse(data);
-									     if(pdata.hasOwnProperty('cli_id'))
-										 context.cli_id = pdata.cli_id;
-									     _incoming.add(pdata.msg);
-									 }
-									 request.close(); //в будущем надо учесть переиспользование объекта, возможно:)
-								     });
-						     request.on_error(function(e){
-									  if(--_holder.success_metr == 1){
-									      _lpoller.stop();
-									      _holder.on_disconnect(e);
-									  }
-								      });
+
+						     request.on_recv(_incoming.get_process_msg(request, _holder));
+
 						     request.open(context);
 						     if(_packets.length)
 							 request.send(_packets.shift());
@@ -105,49 +118,52 @@ function lpoller(context, _holder, _incoming, modules){
 							 request.send(JSON.stringify({'cli_id' : context.cli_id}));
 						 }else{
 						     console.log('lpoller.try_poll: cannot create request');
-						     console.log(_packets);
+						     console.log("packets is: " + _packets);
 						 }
 					     }, 200, true);	
 	}
     }
 
-    this.stop = function(){
+    this.deactivate = function(){
 	if(typeof(_timer) == 'object'){
-	    _timer.destroy();
-            _timer = undefined;	    
+	    poll_timer.destroy();
+            poll_timer = null;	    
 	}
     }
 }
 
 exports.create = function(context, type, modules){
-    var _incoming = new utils.msg_queue();
-    //реализовать выбор транспорта, xhr или script
+    var _incoming = new incoming_processor(context);
+    //реализовать выбор транспорта, xhr или script для browser
     var _holder = new requests_holder(type, modules);
-    context.cli_id = 0;
     var _lpoller = new lpoller(context, _holder, _incoming, modules);
+    _holder.lpoller = _lpoller;
     var _sender = new packet_sender(context, _holder, _incoming, _lpoller, modules);
+
+    context.cli_id = 0; //need connect for id allocating
     var _on_recv = function(){};
+
     return {
 	'connect' : function(callback){
-	    _incoming.on_add(function(msg){
-				 //waiting for server answer with allocated id for us
-				 _incoming.on_add(_on_recv);
-				 _lpoller.try_poll();
-				 callback();
-	    });
-	    _sender.send({});
+	    _incoming.on_recv(function(msg){
+				  _incoming.on_recv(_on_recv);
+				  _lpoller.try_poll();
+				  callback();
+			      });
+	    _sender.send({}); //sending blank msg with cli_id==0 as connect msg
 	},
 	'send' : function(msg){
 	    _sender.send(msg);
 	},
 	'on_recv' : function(callback){
-	    _incoming.on_add(_on_recv = callback);
+	    _incoming.on_recv(_on_recv = callback);
 	},
 	'on_disconnect' : function(callback){
 	     _holder.on_disconnect = callback;
 	},
 	'disconnect' : function(){
-	    _lpoller.stop();
+	    _holder.close_all_requests();
+	    _lpoller.deactivate();
 	}
     }
 }
