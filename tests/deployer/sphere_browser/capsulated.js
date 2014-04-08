af28a69d86c6170639ce546ce70088f52fb725c9
Nikita Zaharov aka ix <ix@2du.ru>
2014-04-09 01:56:48 +0600

    sphere: adding action_panel to configuration and loading

--- tests/deployer/sphere_browser/capsulated.js
+++ tests/deployer/sphere_browser/capsulated.js
@@ -19,117 +19,23 @@ exports.main = function(env){
 
     var ui = {
     }
+    ui.label = sloader.load('dsa/services/ui/overlay/label', mqnode1, env);    
     ui.button = sloader.load('dsa/services/ui/overlay/button', mqnode1, env);    
     ui.entry = sloader.load('dsa/services/ui/overlay/entry', mqnode1, env);
     ui.panel = sloader.load('dsa/services/ui/overlay/panel', mqnode1, env);
     
     var address_panel = sloader.load('sphere/ui/address_panel', mqnode1, env);
+    var action_panel = sloader.load('sphere/ui/action_panel', mqnode1, env);
 
     var seq = capsule.modules.sequence;
     seq.mq_send = _mq.send;
 
     seq.run([
-		   {
-		      action : ['s', address_panel, 'create', ui]
-		   }
-	       ]);
-
-    
-/*
-    var ui_sprout = [
-	{
-	    name : 'panel',
-	    
-	    action : ['s', panel, 'create',
-		      {
-			  "x" : '5%',
-			  "y" : '5%',
-			  "width" : "80%" ,
-			  "height" : "80%",
-			  "orientation" : "bottom"
-		      }],
-	    
-	    next : [
 		{
-		    action : ['s', swiper, 'create', {
-				  x : 0,
-				  y : 0,
-				  width : '100%',
-				  height : '100%',
-			      },
-			      'panel']  
+		    action : ['s', address_panel, 'create', ui]
 		},
 		{
-		    action : ['s', button, 'create', {
-				  "label" : "go",
-				  "x" : "79%",
-				  "y" : "2%",
-				  "width" : "20%",
-				  "height" : "20%",
-				  
-				  "on_pressed" : [
-				      {
-					  action : ['f', function(stack, sprout_pusher){
-							stack.push(stack.last = 'ttt');
-						    }]
-				      },
-				      {
-					  action : ['f', function(stack, sprout_pusher){
-							console.log(stack.last);
-							console.log('eeee');
-						    }]
-				      }
-				  ]
-			      },
-			      'panel'
-		    ]		    
-		},
-		{
-		    action : ['s', button, 'create',{
-				  "label" : "uhlala",
-				  "x" : "79%",
-				  "y" : "78%",
-				  "width" : "20%",
-				  "height" : "20%",
-				  
-				  "on_pressed" : [
-				      {
-					  action : ['f', function(stack, sprout_pusher){
-							console.log('gobutton is pressed');
-						    }]	  
-				      }
-				  ]
-			      }, 'panel'
-			     ]	    
-		},
-		{
-		    action : ['s', entry, 'create',
-			      {
-				  "x" : "1%",
-				  "y" : "2%",
-				  "width" : "77%",
-				  "height" : "20%",
-				  
-				  "on_text_change" : [
-				      {
-					  action : ['f', function(stack, sprout_pusher){
-							console.log(stack.text);
-						    }]
-				      }
-				  ]
-			      }, 'panel'
-			     ]    
+		    action : ['s', action_panel, 'create', ui]   
 		}
-	    ]
-	}
-    ]
-
-    seq.run(ui_sprout);
-*/
-/*    var sid = sloader.load('tests/test_set/service_one', mqnode1, env);    
-    _mq.send(sid, [null, "set", "gg", "ttte"]);
-    _mq.send(sid, [null, "ping", "ttt"]);
-    _mq.send(sid, [null, "pong", "tttg"]);
-    _mq.send(sid, [null, "init", "ttta"]);
-*/
+	    ]);
 }
