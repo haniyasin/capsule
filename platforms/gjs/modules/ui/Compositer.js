5bc0bc22735b974ae6c372d8d7cb0c0668585ab6
Nikita Zaharov aka ix <ix@2du.ru>
2014-06-08 22:01:25 +0600

    gjs Compositer arhitecture is almost completed.
    + Need work to divert implementation on several files. Parts for this already is ready
    + Basic functionality to change elements' properties is done
    + relative size and position with using percents is almost done, need work
      to support deep nested frames

--- platforms/gjs/modules/ui/Compositer.js
+++ platforms/gjs/modules/ui/Compositer.js
@@ -12,6 +12,12 @@ function frame(){
 				     var element = new element_obj_proto(new Clutter.Actor(), info);
 				     element.childs = [];
 				     element.actor.show();
+				     var color = new Clutter.Color();
+				     color.alpha = 200;
+				     color.blue = Math.random() * 255;
+				     color.red = Math.random() * 255;
+				     color.green = Math.random() * 255;
+				     element.actor.set_background_color(color);
 				     return elements.put(element);
 				 },
 				 destroy : function(id){
@@ -19,13 +25,11 @@ function frame(){
 				 },
 				 add : function(parent_id, child_id){
 				     var child = elements.take(child_id);
-				     if(!parent_id)
-					 this.root_actor.add_actor(child.actor);
-				     else {
-					 var parent = elements.take(parent_id);
-					 parent.childs.push(child);
-					 child.parent = parent;
-				     }
+				     var parent = elements.take(parent_id);
+				     parent.childs.push(child);
+				     child.parent = parent;
+				     parent.actor.add_actor(child.actor);
+				     child.props_manager.apply_all();
 				 }
 			     });
 }
@@ -80,6 +84,7 @@ function element(){
 				 change_props : function(id, info){
 				     var element = elements.take(id);
 				     element.set_all(info);
+				     element.apply_all();
 				 }
 			     });
 }
@@ -98,18 +103,36 @@ function animation(){
 			     });
 }
 
+function event(){
+    return new element_proto('event', {
+				 register : function(element_id, event_name){
+				 },
+				 unregister : function(element_id, event_name){
+				 }
+			     });
+}
+
 function element_proto(name, props){
     this.name = name;
     this.props = props;    
 }
 
-function elem_obj_props(element){
+function props_manager(element){
     var types = {
 	x : {
 	    set : function(value){
 		if(typeof value == undefined)
 		    value = 0;
+
+		element.props.x = value;
+	    },
+	    apply : function(){
+		var value = element.props.x;
 		//проверяем не в процентах ли, конвертируем
+		var percent_result = /^(\d{0,2})%/.exec(value);
+		if(percent_result != null){
+		    value = element.parent.props.width / 100 * percent_result[1];		    
+		}
 		element.actor.set_x(value);
 	    }
 	},
@@ -117,6 +140,15 @@ function elem_obj_props(element){
 	    set : function(value){
 		if(typeof value == undefined)
 		    value = 0;
+		element.props.y = value;
+	    },
+	    apply : function(){
+		var value = element.props.y;
+		//проверяем не в процентах ли, конвертируем
+		var percent_result = /^(\d{0,2})%/.exec(value);
+		if(percent_result != null){
+		    value = element.parent.props.height / 100 * percent_result[1];		    
+		}
 		element.actor.set_y(value);
 	    }
 	},
@@ -124,6 +156,15 @@ function elem_obj_props(element){
 	    set : function(value){
 		if(typeof value == undefined)
 		    value = 0;
+		element.props.width = value;
+	    },
+	    apply : function(){
+		var value = element.props.width;
+		//проверяем не в процентах ли, конвертируем
+		var percent_result = /^(\d{0,2})%/.exec(value);
+		if(percent_result != null && percent_result.length == 2){
+		    value = element.parent.props.width / 100 * percent_result[1];		    
+		}
 		element.actor.set_width(value);
 	    }
 	},
@@ -131,6 +172,15 @@ function elem_obj_props(element){
 	    set : function(value){
 		if(typeof value == undefined)
 		    value = 0;
+		element.props.height = value;
+	    },
+	    apply : function(){
+		var value = element.props.height;
+		//проверяем не в процентах ли, конвертируем
+		var percent_result = /^(\d{0,2})%/.exec(value);
+		if(percent_result != null && percent_result.length == 2){
+		    value = element.parent.props.height / 100 * percent_result[1];		    
+		}
 		element.actor.set_height(value);
 	    }
 	},
@@ -138,7 +188,10 @@ function elem_obj_props(element){
 	    set : function(value){
 		if(typeof value == undefined)
 		    value = 1;
-		element.actor.set_opacity(value * 255);
+		element.props.opacity = value;
+	    },
+	    apply : function(){
+		element.actor.set_opacity(element.props.opacity * 255);
 	    }
 	}
     };
@@ -151,17 +204,25 @@ function elem_obj_props(element){
 		types[type].set();
 	}
     };
+
+    this.apply_all = function(){
+	for(prop in element.props){
+	    types[prop].apply();
+	}
+    };
 }
 
 function element_obj_proto(actor, info){
     this.actor = actor; //Clutter actor
-    this.props = new elem_obj_props(this);
-    this.props.set_all(info, true);
+    this.props_manager = new props_manager(this);
+    this.props = {};
+    this.props_manager.set_all(info, true);
 }
 
+//Pool implementation has been taked from web Compositer by freeze
 var Pool = function () {
     this.pool = []; this.available = []; this.count = 0;
-    
+ 
     return undefined;
 };
 
@@ -215,12 +276,15 @@ manager.add(image);
 manager.add(text);
 manager.add(button);
 manager.add(entry);
+manager.add(element);
+manager.add(animation);
+manager.add(event);
 
 function comp(){
     manager.assemble(this);
 
     this.root = new Gtk.Window({ type : Gtk.WindowType.TOPLEVEL });
-    this.root.title = 'GJS compositer module prototype';
+    this.root.title = 'GJS compositer prototype';
     this.root.show();
 
     var actor = new GtkClutter.Embed();
@@ -234,6 +298,21 @@ function comp(){
     color.red = 240;
     color.green = 250;
     stage.set_background_color(color);
+    this.frame_create({ x : 50, y : 50, width : 400, height : 300, opacity : 0.9 });
+    var element = elements.take(0);
+    element.props_manager.apply_all();
+    this.root_actor.add_actor(element.actor);
+
+    var timeline = new Clutter.Timeline({duration : 1000});
+    var rotation = 0;
+
+    timeline.connect('new-frame', function() {
+			 rotation += 0.3;
+			 element.actor.set_rotation(Clutter.RotateAxis.Z_AXIS, rotation, 200,0,0);
+		     });
+    timeline.set_loop(true);
+    timeline.start();
+
 //    print(JSON.stringify(this));
 }
 
--- tests/modules/ui/Compositer.js
+++ tests/modules/ui/Compositer.js
@@ -7,7 +7,7 @@ exports.test = function(capsule){
     comp.frame_add(0, image);
     image = comp.image_create({ x : 40, y : 40, width : 50, height : 50, opacity : 0.8 });
     comp.frame_add(0, image);
-    image = comp.image_create({ x : 70, y : 70, width : 50, height : 50, opacity : 0.8 });
+    image = comp.image_create({ x : "10%", y : "10%", width : "88%", height : "88%", opacity : 0.8 });
     comp.frame_add(0, image);
     image = comp.image_create({ x : 100, y : 100, width : 50, height : 50, opacity : 0.8 });
     comp.frame_add(0, image);
