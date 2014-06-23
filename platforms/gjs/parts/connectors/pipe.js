5a32c4a416296699a44c0229b25db1d192193e93
Nikita Zaharov aka ix <ix@2du.ru>
2014-06-23 21:41:45 +0600

    gjs part connector almost done and tested by mplayer loading

--- platforms/gjs/parts/connectors/pipe.js
+++ platforms/gjs/parts/connectors/pipe.js
@@ -1,34 +1,55 @@
 const GLib = imports.gi.GLib;
-
+const Gio = imports.gi.Gio;
 exports.create = function(argv, working_directory){
     var _on_out = function(string){
-	print(string);
+//	print(string);
     },
     pid = null,
+    proc_in_unix = null,
     proc_in = null,
-    proc_out = null;
-
+    proc_out = null,
+    proc_out_unix = null,
+    proc_out_source = null;
     return {
 	spawn : function(){
-	  var retobj = GLib.spawn_async_with_pipes(null, argv, ['capsule=yes'], GLib.SpawnFlags.FILE_AND_ARGV_ZERO, function(){});
+	  var retobj = GLib.spawn_async_with_pipes(null, argv, null, GLib.SpawnFlags.SEARCH_PATH, function(){});
 	    pid = retobj[1];
-	    print(JSON.stringify(retobj));
-	    proc_in = GLib.IOChannel.unix_new(retobj[2]);
-	    proc_out = GLib.IOChannel.unix_new(retobj[3]);
-	    GLib.io_add_watch(proc_out, 2, GLib.IOCondition.IN | GLib.IOCondition.HUP, function(channel, condition){
-				  print('haha');
-				   _on_out(proc_out.read_line_string()[1]);
-			       });
+	    proc_in_unix = new Gio.UnixOutputStream({fd : retobj[2]});
+	    proc_in = new Gio.DataOutputStream({ base_stream: proc_in_unix });
+	    proc_out_unix = new Gio.UnixInputStream({fd: retobj[3]});
+	    proc_out = new Gio.DataInputStream({ base_stream: proc_out_unix });
+	    proc_out_source = proc_out_unix.create_source(null);
+	    proc_out_source.attach(null);
+	    var read_flag = true;
+	    proc_out_source.set_callback(function(stream){
+					     if(read_flag){
+						 read_flag = false;
+						 proc_out.read_line_async(1, null, function(stream, result){
+									      _on_out(proc_out.read_line_finish(result)[0]);
+									      read_flag = true;
+									      print(result);
+								      });
+					     }
+					     return true;
+				});
 	},
 
 	in : function(string){
-	    proc_in.write(string);
+	    print(proc_in_unix.write(string, null));
+	    proc_in.flush(null);
 	},
 
 	on_out : function(callback){
+	    _on_out = callback;
 	},
 	destroy : function(){
+	    GLib.spawn_close_pid(pid);
 	    proc_in.unref();
+	    proc_in_unix.unref();
+	    proc_out.unref();
+	    proc_out_unix.unref();
+	    proc_out_source.destroy();
+	    proc_out.unref();
 	}
     };
 };
--- tests/deployer/gjs/capsulated.js
+++ tests/deployer/gjs/capsulated.js
@@ -10,9 +10,19 @@ exports.main = function(env){
 
 //    capsule.tests.modules.ui.Compositer.test(capsule);
 
-    var mplayer = capsule.parts.connectors.pipe.create(['/usr/bin/mplayer', 'http://docs.gstreamer.com/media/sintel_trailer-480p.webm'], null);
-
+    var mplayer = capsule.parts.connectors.pipe.create(['mplayer', '-slave', 'http://docs.gstreamer.com/media/sintel_trailer-480p.webm'], null);
+    
+    mplayer.on_out(function(content){print(content)});
     mplayer.spawn();
+    let GLib = imports.gi.GLib;
+    mplayer.in('pause\n');
+    mplayer.in('pausing\n');
+    mplayer.in('pausing\n');
+    GLib.timeout_add(1, 8000,function(){
+			 mplayer.in('pause\n');
+			 print('haha');
+		     });
+//    mplayer.destroy();
 //    var thttp = capsule.tests.modules.transport.http.client;
     
 //    thttp.test({ 'url' : 'http://localhost:8810/krevetk/o', 'method' : 'POST'}, capsule);
