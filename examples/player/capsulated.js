46d95ab8175a482d2d298ea5efed89ceb4a01d01
Nikita Zaharov aka ix <ix@2du.ru>
2015-02-07 15:04:09 +0500

    дальнейшая переработка examples/player
    + работа над выделением частей проекта - player, controls, file_opener, dnd,
      которые можно будет использовать независимо друг от друга в других проектах
      или в такой связке, но заменяя части из них. Дальнейшая работа будет вестись
      над выделением их в отдельные модули.
    
    + повышение читаемости кода, уход от прототипной лапши к некоему подобию ООП,
      хотя вообще ни в capsule, ни в caravan ООП не является эталонным это уж
      точным.

--- examples/player/capsulated.js
+++ examples/player/capsulated.js
@@ -2,15 +2,49 @@
  * Example Player application based on capsule API(ui, io)
  */
 
-function file_open_form(comp, parent_frame){
-    var form_frame = this.frame = comp.frame_create({
-					   x : '0%',
-					   y : '-90%',
-					   width : '100%',
-					   height : '90%',
-					   opacity : 1,
-					   z_index : 1
-				       }),
+function slide_animation(comp){
+    this.objects = [];
+
+    var anim_down = comp.anim_create([
+					       {
+						   duration : 300,
+						   actions : {
+						       y : 90   
+						   }
+					       }
+					   ]),
+    anim_up = comp.anim_create([
+					 {
+					     duration : 300,
+					     actions : {
+						 y : -90
+					     } 
+					 }
+				     ]);
+    function hup(){
+	comp.anim_start(this.banimslu);	
+    }
+
+    function hdown(){
+	comp.anim_start(this.banimsld);		
+    }
+    this.bind = function(widget){
+	widget.anim = {
+	    banimslu : comp.anim_bind(widget.frame, anim_up),
+	    banimsld : comp.anim_bind(widget.frame, anim_down),
+	    up : hup,
+	    down : hdown
+	};
+    };
+
+    this.unbind = function(widget){
+	comp.anim_unbind(widget.anim.banimslu);
+	comp.anim_unbind(widget.anim.banimsld);
+    };
+}
+
+function file_opener_widget(comp, parent, info){
+    var form_frame = this.frame = comp.frame_create(info),
     bg = comp.image_create({
 			       x : '0%',
 			       y : '0%',
@@ -69,9 +103,9 @@ function file_open_form(comp, parent_frame){
     addrc.set_value("http://docs.gstreamer.com/media/sintel_trailer-480p.ogv");
     comp.frame_add(form_frame, bg);
     comp.frame_add(form_frame, text);
-    comp.frame_add(form_frame, addre);
     comp.frame_add(form_frame, okb);
-    comp.frame_add(parent_frame, form_frame);
+    comp.frame_add(parent.frame, form_frame);
+    parent.file_opener = this;
     
     okc.on_press(function(){
 		     comp.anim_start(banimhide);
@@ -85,17 +119,8 @@ function file_open_form(comp, parent_frame){
     };
 }
 
-function controls_widget(comp, player, video, info){
-    var playb = comp.button_create({
-				       x : '0%',
-				       y : '0%',
-				       width : '10%',
-				       height : '100%',
-				       z_index : 2,
-				       opacity : 0.9,
-				       label : 'play'
-				   }),
-    frame_timeline = comp.frame_create(
+function controls_widget(comp, player, parent, info){
+    var frame_timeline = comp.frame_create(
 	{
 	    width : '89%',
 	    height : '100%',
@@ -104,14 +129,22 @@ function controls_widget(comp, player, video, info){
 	    z_index : 2
 	}
     ),
+    playb = comp.button_create(
+	{
+	    x : '0%',
+	    y : '0%',
+	    width : '10%',
+	    height : '100%',
+	    z_index : 2,
+	    opacity : 0.9,
+	    label : 'play'
+	}),
     image_timeline = comp.image_create(
         {
             width : '100%',
             height : '100%',
-
             x : '0%',
             y : '0%',
-
             z_index : 2,
 
             source : require('images/blue')
@@ -124,10 +157,11 @@ function controls_widget(comp, player, video, info){
 	    height : '100%', 
 	    opacity : 0.8,
 	    z_index : 2,
+
 	    source : require('images/red')
-	}),
-    vcontrol = comp.video_get_control(video);    
+	});
     this.frame = comp.frame_create(info);
+    comp.frame_add(parent.frame, this.frame);
     comp.frame_add(this.frame, playb);
     comp.frame_add(this.frame, frame_timeline);
     comp.frame_add(frame_timeline, image_timeline);
@@ -137,40 +171,35 @@ function controls_widget(comp, player, video, info){
     var pause = true;
 
     function play(){
-	vcontrol.play();
+	player.vcontrol.play();
 	playc.set_label('pause');
 	pause = false;				  			   
     }
     playc.on_press(function(){
 		       if(pause){
-			   if(source == null){
-			       addr_form.setup(function(address){
-						   source = address;
-						   vcontrol.load(source);
-						   player.up();
-						   addr.up();
-//						   comp.anim_start(player_banimslu);
-						   //						   comp.anim_start(addr_banimslu);
-						   play();
-					       });
-			       player.down();
-			       addr.down();
-//			       comp.anim_start(player_banimsld);
-//			       comp.anim_start(addr_banimsld);
+			   if(player.source == null){
+			       player.file_opener.setup(function(address){
+							    player.source = address;
+							    player.vcontrol.load(player.source);
+							    player.anim.up();
+							    play();
+							});
+			       player.anim.down();
 			       return;
 			   }
 			   play();
 		       } else {
-			   vcontrol.pause();
+			   player.vcontrol.pause();
 			   playc.set_label('play');
 			   pause = true;
 		       }
 		   });
+
     var step, prev_step = 0, position_step;;
-    vcontrol.on_timeupdate(function(){
+    player.vcontrol.on_timeupdate(function(){
 			       if(typeof(position_step) == 'undefined')
-				   position_step = vcontrol.get_duration() / 200;
-			       step = Math.round(vcontrol.get_position() / position_step)/2;
+				   position_step = player.vcontrol.get_duration() / 200;
+			       step = Math.round(player.vcontrol.get_position() / position_step)/2;
 			       if(step > prev_step || step < prev_step){
 				   var inc_anim = comp.anim_create([
 								       {
@@ -211,7 +240,7 @@ function controls_widget(comp, player, video, info){
 			    point_drag = false;
 			    step = pointer_obj.shift().x;
 			    ///			    print('eee', prev_step, step, (vcontrol.get_duration() / 100) * step);
-			    vcontrol.set_position((vcontrol.get_duration() / 100) * step);
+			    player.vcontrol.set_position((player.vcontrol.get_duration() / 100) * step);
 			    var timepoint_slide_anim = comp.anim_create([
 									    {
 										duration : 300,
@@ -225,10 +254,8 @@ function controls_widget(comp, player, video, info){
 			});
 }
 
-function video_player(comp){
-    var player_frame = comp.frame_create({ x : '0%', y : '0%', 
-					   width : '100%', height : '100%', 
-					   z_index : 2}),
+function player_widget(comp, parent, info){
+    var player_frame = this.frame = comp.frame_create(info),
     bg_image = comp.image_create({
 				     x : '0%', y : '0%',
 				     width : '100%', height : '100%',
@@ -237,53 +264,22 @@ function video_player(comp){
 				}),
     video = comp.video_create({ x : '0%', y : '1%', 
 				width : '100%', height : '89%', 
-				z_index : 2, opacity : 1}),
-    drag_dest = comp.dnd_destination_create({
-						x : '0%',
-						y : '0%',
-						width : '100%',
-						height : '100%',
-						opacity : 0.1,
-						z_index : 1
-					    }, null, null, null),
-    vcontrol = comp.video_get_control(video),
-    addr_form = new file_open_form(comp, 0),
-    source = null,
-    controls = new controls_widget(comp, video, {
-				x : '1%',
-				y : '88%',
-				width : '98%',
-				height : '10'
-			    });    
+				z_index : 2, opacity : 1});
+    this.vcontrol = comp.video_get_control(video),
+    this.vcontrol.set_volume(0.2);
+    this.source = null;
 
     comp.frame_add(player_frame, bg_image);
     comp.frame_add(player_frame, video);
-    comp.frame_add(player_frame, controls.frame);
-    comp.frame_add(player_frame, drag_dest.id);
-    comp.frame_add(0, player_frame);
+    comp.frame_add(parent.frame, player_frame);
+}
 
-    var anim_slide_down = comp.anim_create([
-					       {
-						   duration : 300,
-						   actions : {
-						       y : 90   
-						   }
-					       }
-					   ]),
-    anim_slide_up = comp.anim_create([
-					 {
-					     duration : 300,
-					     actions : {
-						 y : -90
-					     } 
-					 }
-				     ]),
-    player_banimslu = comp.anim_bind(player_frame, anim_slide_up),
-    player_banimsld = comp.anim_bind(player_frame, anim_slide_down),
-    addr_banimslu = comp.anim_bind(addr_form.frame, anim_slide_up),
-    addr_banimsld = comp.anim_bind(addr_form.frame, anim_slide_down);
+function dnd_widget(comp, player, info){
+    var drag_dest = comp.dnd_destination_create(info, null, null, null);
+    this.frame = drag_dest.id;
 
-    
+    comp.frame_add(player.frame, drag_dest.id);
+
     drag_dest.on('motion', function(context, x, y){
 		     return true;
 		 });
@@ -291,11 +287,8 @@ function video_player(comp){
 		     return true;
 		 });
     drag_dest.on('data', function(context, x, y){
-		     source = context.data;
-		     vcontrol.load(source);
+		     vcontrol.load(player.source = context.data);
 		 });
-
-    vcontrol.set_volume(0.2);
 }
 
 exports.main = function(){
@@ -303,6 +296,36 @@ exports.main = function(){
     require('modules/ui/dnd');//подключаем возможности dnd в Compositer
 
 //    print(JSON.stringify(Compositer));
-    var comp = Compositer.create();
-    video_player(comp);
+    var comp = Compositer.create(),
+    sanim = new slide_animation(comp),
+    player = new player_widget(comp, {frame : 0}, {
+				   x : '0%', y : '0%', 
+				   width : '100%', height : '100%', 
+				   z_index : 2
+			       }),
+    file_opener = new file_opener_widget(comp, player, {
+					     x : '0%',
+					     y : '-90%',
+					     width : '100%',
+					     height : '90%',
+					     opacity : 1,
+					     z_index : 1
+					 }),
+    controls = new controls_widget(comp, player, player, {
+				       x : '1%',
+				       y : '88%',
+				       width : '98%',
+				       height : '10'
+			    }),
+    dnd = new dnd_widget(comp, player, {
+			     x : '0%',
+			     y : '0%',
+			     width : '100%',
+			     height : '80%',
+			     opacity : 0.1,
+			     z_index : 1
+			 });    
+
+    sanim.bind(player);
+    sanim.bind(file_opener);
 };
