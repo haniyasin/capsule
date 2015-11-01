86f463f264e9ef547f6da11e3400e9a23c93db5f
Nikita Zaharov aka ix <ix@2du.ru>
2015-11-02 00:00:27 +0500

    новая глава в tutor и исправлена орфографическая ошибка в modules/timer

--- modules/timer.js
+++ modules/timer.js
@@ -1,11 +1,11 @@
 var error = require('parts/error');
 
-exports.create = function(callback, milisec, cyclic){
+exports.create = function(callback, millisec, cyclic){
     if(typeof(setInterval) == 'undefined'
        &&typeof(setTimeout) == 'undefined')
 	return new error('not supported', 'this platform is not supported timer functionality');
  
-    var id = cyclic ? setInterval(callback, milisec) : setTimeout(callback, milisec);
+    var id = cyclic ? setInterval(callback, millisec) : setTimeout(callback, millisec);
     return {
 	destroy : function(){
 	    cyclic ? clearInterval(id) : clearTimeout(id);
